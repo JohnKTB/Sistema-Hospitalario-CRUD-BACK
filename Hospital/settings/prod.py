c798443d9e8eb4d2983f27138fa3c991cf8b7369
John <johnrenato_8@hotmail.com>
2020-12-03 19:57:45 -0500

    agregando requirements

--- Hospital/settings/prod.py
+++ Hospital/settings/prod.py
@@ -10,9 +10,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'mysql_cymysql',
-        'NAME': 'dbhospital',
-        'USER': 'root',
-        'PASSWORD': 'samanthafox',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
